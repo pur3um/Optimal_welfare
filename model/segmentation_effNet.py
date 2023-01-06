931dbf5c2818e54335b8d50370fdcc62e5d49567
pur3um <pureum030@gmail.com>
2023-01-06 17:54:05 +0900

    파일 변경 및 인수 추가

--- model/segmentation_effNet.py
+++ model/segmentation_effNet.py
@@ -5,8 +5,9 @@ from keras.models import Sequential
 from keras.optimizers import Adam
 from math import ceil
 from time import time
-import keras.backend.tensorflow_backend as K
+import keras.backend as K
 import tensorflow as tf
+from tensorflow_addons.layers import GroupNormalization
 import efficientnet.keras as efn
 import numpy as np
 import argparse
@@ -18,6 +19,7 @@ import csv
 import os
 
 """
+https://keras.io/api/applications/efficientnet/
 왜 EfficientNet을 사용했는지, : effnet의 장점
 데이터의 포맷과 관련해서 버전에 왜 이게 더 맞는지
 - efficientNet은 inputsize = 600
@@ -56,6 +58,22 @@ class EFNet():
                             classifier_activation="softmax",
                             **kwargs
                         )
+
+        인수
+        include_top : 네트워크 상단에 완전 연결 계층을 포함할지 여부. 기본값은 True입니다.
+        weights : None(무작위 초기화), 'imagenet'(ImageNet에 대한 사전 교육) 또는 로드할 가중치 파일의 경로 중 하나입니다.
+        기본값은 '이미지넷'입니다.
+        input_tensorlayers.Input() : 모델의 이미지 입력으로 사용할 선택적 Keras 텐서(예: 의 출력 ).
+        input_shapeinclude_top : 선택적 모양 튜플, False 인 경우에만 지정됩니다 . 정확히 3개의 입력 채널이 있어야 합니다.
+        pooling : 기능 추출을 위한 선택적 풀링 모드 include_top입니다 False. 기본값은 없음입니다.
+         - None모델의 출력이 마지막 컨볼루션 레이어의 4D 텐서 출력임을 의미합니다.
+          - avg전역 평균 풀링이 마지막 컨볼루션 레이어의 출력에 적용되므로 모델의 출력은 2D 텐서가 됩니다.
+           - max전역 최대 풀링이 적용됨을 의미합니다.
+        classes : 이미지를 분류할 선택적 클래스 수이며 include_top, True인 경우와 weights인수가 지정되지 않은 경우에만 지정됩니다.
+         기본값은 1000(ImageNet 클래스 수)입니다.
+        classifier_activation : A str또는 호출 가능. "최상위" 레이어에서 사용할 활성화 함수입니다. 가 아니면 무시됩니다 include_top=True.
+         classifier_activation=None"최상위" 레이어의 로짓을 반환하도록 설정 합니다. 기본값은 'softmax'입니다.
+          사전 훈련된 가중치를 로드할 때 는 또는 classifier_activation이어야 합니다 .None"softmax"
         """
         effnet = efn.EfficientNetB4(weights=None,
                                     include_top=False,
